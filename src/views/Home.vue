7751a241709f53fb0918ff2aa21d438666964664
Jorge Medina Sistemas <jorge.si.med@gmail.com>
2020-01-01 21:41:08 -0600

    Configurando el proyecto, instalando las librerias y hola mundo de Vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <v-layout :wrap="true">
+    <v-flex xs12>
+      <v-card color="error" dark>
+        <v-card-text class="display-1 text-sm-center">
+          650
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
